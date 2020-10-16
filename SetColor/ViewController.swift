4cf16c7af99c14200eee52db3b18f2d33214b5c3
Filipp Slabodetsky <oker211@gmail.com>
2020-10-16 17:32:49 +0300

    Initial Commit

---
+++ SetColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SetColor
+//
+//  Created by Филипп Слабодецкий on 16.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
